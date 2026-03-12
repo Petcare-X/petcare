2457d86b9359e3e7554a464dc5c0e63c961c8aac
sskillaxx <maks.kutimskiy@gmail.com>
2026-03-12 11:52:39 +0300

    added config JWT and TG

--- apps/backend/src/core/config.py
+++ apps/backend/src/core/config.py
@@ -1,8 +1,14 @@
+from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
     DATABASE_URL: str
+    JWT_SECRET_KEY: str = Field(min_length=32)
+    JWT_ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    REFRESH_TOKEN_EXPIRE_DAYS: int = 7
+    TELEGRAM_BOT_TOKEN: str | None = None
 
 settings = Settings()
