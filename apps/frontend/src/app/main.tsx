475934f71f2fff39ba34718df19a215f40164f34
sskillaxx <maks.kutimskiy@gmail.com>
2026-04-14 13:30:27 +0300

    created home-page structure (without css)

--- apps/frontend/src/app/main.tsx
+++ apps/frontend/src/app/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import { Providers } from "@/app/providers";
+import HomePage from "@/pages/home/home-page";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
+        <HomePage />
         <Providers />
     </StrictMode>
 );
